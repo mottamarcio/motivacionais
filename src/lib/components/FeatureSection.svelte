6af16b2034628e2f0e6f58b58572dcd839e53d50
Marcio Motta <marcio.motta@hp.com>
2025-11-04 17:19:36 -0300

    update text from feature section

--- src/lib/components/FeatureSection.svelte
+++ src/lib/components/FeatureSection.svelte
@@ -14,14 +14,14 @@
         <div class="w-16 h-16 rounded-full mx-auto mb-4 flex items-center justify-center bg-teal-100">
             <BicepsFlexed size={30} color="teal"/>
         </div>
-        <h3 class="text-xl font-medium mb-2">Inspiração Diária</h3>
-        <p class="text-gray-600">Novas frases todos os dias para renovar sua energia</p>
+        <h3 class="text-xl font-medium mb-2">Força Interior</h3>
+        <p class="text-gray-600">Palavras que despertam a coragem que você esqueceu que tinha</p>
     </div>
     <div class="text-center">
         <div class="w-16 h-16 rounded-full mx-auto mb-4 flex items-center justify-center bg-teal-100">
             <Moon size={30} color="teal"/>
         </div>
-        <h3 class="text-xl font-medium mb-2">Inspiração Diária</h3>
-        <p class="text-gray-600">Novas frases todos os dias para renovar sua energia</p>
+        <h3 class="text-xl font-medium mb-2">Serenidade</h3>
+        <p class="text-gray-600">Mensagens que te lembram que calma também é força</p>
     </div>
 </div>
