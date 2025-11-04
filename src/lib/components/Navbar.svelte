31f342ad1a9523ca6911a418b79de660d2d1495b
Marcio Motta <marcio.motta@hp.com>
2025-11-04 13:54:21 -0300

    add initial Navbar version

---
+++ src/lib/components/Navbar.svelte
@@ -0,0 +1,32 @@
+<script>
+	import { Quote, LayoutGrid, ChevronDown } from '@lucide/svelte';
+</script>
+
+<nav class="relative flex items-center justify-between max-w-6xl mx-auto px-6 py-8">
+	<div class="flex items-center gap-2">
+		<div class="w-10 h-10 bg-teal-100 rounded-full flex items-center justify-center">
+			<Quote size={20} color="teal" />
+		</div>
+		<div class="text-2xl font-thin pb-1">Motivacionais</div>
+	</div>
+	<div>
+		<button class="flex items-center gap-1">
+			<LayoutGrid size={18} color="#333" />
+			<span id="categories" class="pb-1">Categorias</span>
+			<ChevronDown size={16} color="#333" />
+		</button>
+	</div>
+</nav>
+
+<style>
+	/* Responsive */
+	@media (max-width: 768px) {
+        nav {
+            padding: 16px;
+        }
+
+		#categories {
+            display: none;
+        }   
+	}
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,15 @@
 <script lang="ts">
-	import favicon from '$lib/assets/favicon.svg';
+	import '../app.css';
+	import Navbar from '$components/Navbar.svelte';
 
 	let { children } = $props();
 </script>
 
-<svelte:head>
+<!-- <svelte:head>
 	<link rel="icon" href={favicon} />
-</svelte:head>
+</svelte:head> -->
 
-{@render children()}
+<div class="container mx-auto max-w-6xl">
+	<Navbar />
+	{@render children()}
+</div>
