7a8aea73634fc7fdc64521783fce9e0c154cccce
Marcio Motta <marcio.motta@hp.com>
2025-11-07 11:39:01 -0300

    disable menu from navbar

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -3,19 +3,19 @@
 </script>
 
 <nav class="relative flex items-center justify-between max-w-6xl mx-auto px-6 py-8">
-	<div class="flex items-center gap-2">
+	<div class="flex items-center mx-auto gap-2">
 		<div class="w-10 h-10 bg-teal-100 rounded-full flex items-center justify-center">
 			<Quote size={20} color="teal" />
 		</div>
 		<div class="text-2xl font-thin pb-1">Motivacionais</div>
 	</div>
-	<div>
+	<!-- <div>
 		<button class="flex items-center gap-1">
 			<LayoutGrid size={18} color="#333" />
 			<span id="categories" class="pb-1">Categorias</span>
 			<ChevronDown size={16} color="#333" />
 		</button>
-	</div>
+	</div> -->
 </nav>
 
 <style>
